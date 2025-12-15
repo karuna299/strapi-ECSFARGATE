6839b04665c5dacdd2e3403e215c17b03b76d0ca
karuna <karunaissa400@gmail.com>
2025-12-15 17:12:52 +0000

    added destroy.yml

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "my-terraform-state-karuna"
+    bucket       = "my-terraform-state-6"
     key          = "terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
