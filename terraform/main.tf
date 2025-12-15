27d734631bb95eced08add1135c8a74395f3cb25
karuna <karunaissa400@gmail.com>
2025-12-15 16:26:06 +0000

    edited main.tf rds block

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,7 +83,7 @@ resource "aws_route_table_association" "karuna_public_rta" {
 ##############################################
 
 resource "aws_eip" "karuna_nat_eip" {
-  vpc = true
+  domain = "vpc"
 }
 
 resource "aws_nat_gateway" "karuna_nat" {
@@ -186,14 +186,14 @@ resource "aws_db_instance" "karuna_postgres" {
   engine                 = "postgres"
   instance_class         = var.db_instance_class
   allocated_storage      = 20
-  name                   = var.db_name
+  db_name                = var.db_name
   username               = var.db_username
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.karuna_db_subnet.id
   vpc_security_group_ids = [aws_security_group.karuna_sg_db.id]
 
-  skip_final_snapshot = true
-  publicly_accessible = false
+  skip_final_snapshot     = true
+  publicly_accessible     = false
 }
 
 ##############################################
