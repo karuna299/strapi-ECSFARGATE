1b02b8470d3c13894114b1212ffe745326aef7b4
karuna <karunaissa400@gmail.com>
2025-12-16 09:32:18 +0000

    changed backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "my-terraform-state-6"
+    bucket       = "my-terraform-state-karuna"
     key          = "terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
